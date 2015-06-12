66463a4d91d0b2191f686effb46608195bcb7885
rmaliza <me@ronaldmaliza.com>
2015-06-12 16:52:20 -0500

    updated install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
+
 echo installing
+mv /etc/pki/rpm-gpg /etc/pki/rpm-gpg_source
+mv parachutes-master/rpm-gpg /etc/pki/
+mv /etc/yum.repos.d /etc/yum.repos.d_source
+mv parachutes-master/yum.repos.d /etc/
+yum update -q
+yum -y install $(cat parachutes-master/package_elastix-2.5) 
